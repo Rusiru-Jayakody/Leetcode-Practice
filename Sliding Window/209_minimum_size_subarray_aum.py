644c26eebde382fa813e30ecac43b116e37df4b4
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2025-12-27 23:32:51 +0530

    Solution for 209.minimum size subarray sum

---
+++ Sliding Window/209_minimum_size_subarray_aum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums) -> int:
+        shortest = float('inf')
+        l = 0
+        summ = 0
+        for r in range(len(nums)):
+            summ += nums[r]
+            while summ >= target:
+                shortest = min(shortest, (r-l+1))
+                summ -= nums[l]
+                l += 1
+        return shortest if shortest < float('inf') else 0
+    
+#In the leetcode code editor you dont need to write the following code . It is just to run in your local machine.
+
+if __name__ == "__main__":
+
+    sol = Solution()
+    nums = list(map(int,input().split()))
+    k = int(input())
+    ans = sol.minSubArrayLen(k,nums)
+    print(ans)
+        
