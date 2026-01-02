bc91fa72f6db67fe0522fad46fd710bcee15dbbf
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2026-01-03 00:24:23 +0530

    Added alternative solution in 215.code

--- Heaps( Priority queues )/215_k_th_largest_element_in_an_array.py
+++ Heaps( Priority queues )/215_k_th_largest_element_in_an_array.py
@@ -5,6 +5,18 @@ class Solution:
         for _ in range(len(nums)-k):
             heapq.heappop(nums)
         return nums[0]
+
+
+# Better solution with O(nlogk).
+# import heapq
+# class Solution:
+#     def findKthLargest(self, nums, k: int) -> int:
+#         heap = nums[:k]
+#         heapq.heapify(heap)
+#         for i in range(k,len(nums)):
+#             heapq.heappushpop(heap,nums[i])
+#         return heap[0]
+
     
 #In the leetcode code editor you dont need to write the following code . It is just to run in your local machine.
 
