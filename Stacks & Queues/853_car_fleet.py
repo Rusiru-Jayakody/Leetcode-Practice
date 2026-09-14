b9155837fd0502a1c90511232f1c4be615059dad
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2026-09-14 23:46:10 +0530

    Solution for 853 car fleet

---
+++ Stacks & Queues/853_car_fleet.py
@@ -0,0 +1,25 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        temp = []
+        for i in range(len(position)):
+            temp.append([position[i],speed[i]])
+        temp.sort(key = lambda x: x[0])
+        for i in range(len(temp)):
+            temp[i] = (target - temp[i][0]) / temp[i][1]
+        count = 0
+        stk = []
+
+        for i in range(len(temp)-1,-1,-1):
+            if not stk:
+                stk.append(temp[i])
+                count += 1
+            else:
+                if stk[-1] >= temp[i]:
+                    pass
+                else:
+                    stk.pop()
+                    stk.append(temp[i])
+                    count += 1
+        return count
+            
+        
