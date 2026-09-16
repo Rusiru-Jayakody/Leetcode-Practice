2dcd2dda922007fedca3b28344c859f9e7a70296
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2026-09-16 23:43:12 +0530

    Solution for 21 merge 2 sorted lists

---
+++ Linked Lists/21_merge_2_sorted_lists.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1, list2) :
+        n1,n2 = list1, list2
+        head = ListNode()
+        curr = head
+
+        while n1 and n2:
+            if n1.val <= n2.val:
+                curr.next = n1
+                curr = n1
+                n1 = n1.next
+            else:
+                curr.next = n2
+                curr = n2
+                n2 = n2.next
+        
+        curr.next = n1 if n1 else n2
+
+        return head.next
+
+
+
+
+        
